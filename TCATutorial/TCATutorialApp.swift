d11617e91582dacf0f37fb6e0f7cb1e278c96ab0
hide0101 <hideakikomori0101@gmail.com>
2023-06-07 21:42:03 +0900

    Initial Commit

---
+++ TCATutorial/TCATutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  TCATutorialApp.swift
+//  TCATutorial
+//
+//  Created by hide0101 on 2023/06/07.
+//
+
+import SwiftUI
+
+@main
+struct TCATutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
