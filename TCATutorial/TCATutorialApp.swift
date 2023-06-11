28379a6a47f9530fade7109f5383875c1a38b962
hide0101 <hideakikomori0101@gmail.com>
2023-06-11 10:55:27 +0900

    Change View inside WindowGroup

--- TCATutorial/TCATutorialApp.swift
+++ TCATutorial/TCATutorialApp.swift
@@ -5,13 +5,22 @@
 //  Created by hide0101 on 2023/06/07.
 //
 
+import ComposableArchitecture
 import SwiftUI
 
 @main
 struct TCATutorialApp: App {
+    static let store = Store(
+        initialState: CounterFeature.State(),
+        reducer: {
+            CounterFeature()
+                ._printChanges()
+        }
+    )
+    
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            CounterView(store: TCATutorialApp.store)
         }
     }
 }
